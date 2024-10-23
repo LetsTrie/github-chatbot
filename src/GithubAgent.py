a975ecdc634985b48e168b3ecb9ffe4845fcc53d
letstrie-sa <mdsakib@superannotate.com>
2024-10-23 16:21:25 +0600

    updating the prompt

--- src/GithubAgent.py
+++ src/GithubAgent.py
@@ -12,23 +12,26 @@ class GitHubAgent:
         tool_names = [tool.name for tool in self.tools]
         tools_details = get_tools_summary(self.tools)
         
+        # ** Never fetch data by yourself with GitHub API **. It is strictly prohibited. 
+
+        
         return f"""
 Current timestamp: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
-You are a GitHub Repository Analysis Agent named "Github-chatbot". Answer the following questions as best you can on Github Repository: {self.github_url}.  If the questions relate to the repository, you must strictly rely on the tools provided, as described in {tools_details}. ** Never fetch data by yourself with GitHub API **. It is strictly prohibited.
+You are a GitHub Repository Analysis Agent named "Github-chatbot". 
+Answer the following questions as best you can on Repository: {self.github_url} by using the following tools: {tools_details}. 
+
+Do not fetch data yourself using the GitHub API. Direct API access is strictly prohibited.
 
 Use the following format:
 Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of [{tool_names}]. just mention the tool_name. If you detect the tool, then don't go to observation step. User will execute the tool.
-Action Input: if Action is github_tool, do <Query Classification> (JSON format) PAUSE. otherwise skip.
+Thought: think about which tool should be used based on the user's query
+Action: the action to take, should be one of [{tool_names}]. just mention the tool_name. **Do not proceed to observation.**
+Action Input: if Action is github_tool, do <Query Classification> (JSON format). otherwise skip.
 Observation: the result of the action
 ... (this Thought/Action/Action Input/Observation can repeat N times)
 Thought: I now know the final answer
 Final Answer: the final answer to the original input question
 
-Be more descriptive and user friendly.
-If there is no Action to execute. Then output only the Final Answer. 
-Final output should be precise on the given query. Don't make one understand your internal step.
-Don't mention "Final Answer" in the output. 
+If there is no Action to execute. Then output only the Final Answer (Don't mention "Final Answer" in the output). Your response should be concise, user friendly and directly answer the question, without revealing internal steps or mentioning tool names unnecessarily.
 
 Begin!"""
